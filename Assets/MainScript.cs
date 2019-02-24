2206ba14c09ae1bfad4186b4d4761f0f45f05668
Umar Saeed <umar.saeed@student.oulu.fi>
2019-02-24 04:07:17 +0200

    Initial Commit

---
+++ Assets/MainScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainScript : MonoBehaviour
+{
+	public GameObject registerPanel, loginPanel, profilePanel, listpanel;
+
+	public void OnBtnClick(int panelNo)
+	{
+		ClosePanels ();
+		if (panelNo == 0) {
+			loginPanel.SetActive (true);
+		} else if (panelNo == 1) {
+			profilePanel.SetActive (true);
+		} else if (panelNo == 2) {
+			listpanel.SetActive (true);
+		} else if (panelNo == 3) {
+			registerPanel.SetActive (true);
+		}
+	}
+
+	void ClosePanels()
+	{
+		loginPanel.SetActive (false);
+		registerPanel.SetActive (false);
+		profilePanel.SetActive (false);
+		listpanel.SetActive (false);
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+This project was made for Ubi summer school as a workshop project in Unity 3d.
